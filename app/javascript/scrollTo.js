f64d2f9839d03f2223e1281be613df0df5c656aa
Henderson Pinto <hendersoncode@gmail.com>
2020-03-12 19:18:28 +0100

    Added all the labels, reduced size on paragraphs, added links to the projects buttons, and the scroll interaction of the navbar

---
+++ app/javascript/scrollTo.js
@@ -0,0 +1,21 @@
+const navButtons = document.querySelectorAll('.navbar__link')
+
+const sections = {
+    'about': document.querySelector('.section--about'),
+    'skillset': document.querySelector('.section--skillset'),
+    'projects': document.querySelector('.section--projects')
+}
+
+function scrollTo() {
+
+    console.log(this.dataset.linkto)
+    window.scroll({
+        top: sections[this.dataset.linkto].offsetTop - 30,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+navButtons.forEach(button => {
+    button.addEventListener('click', scrollTo)
+})
--- app/javascript/updatecards.js
+++ app/javascript/updatecards.js
@@ -2,50 +2,54 @@ const skills = document.querySelectorAll('.skill');
 const cardArea = document.querySelector('.section--skillset__cards');
 const cards = document.querySelectorAll('.skillcard');
 const cardContent = document.querySelectorAll('.skillcard__content');
-const labels = document.querySelectorAll('skillcard__label')
+const labels = document.querySelectorAll('skillcard__label');
 let lastClicked;
 
 const technologies = {
 	backend: {
-		title: ['PostgreSQL', 'Node.js', 'Ruby'],
+		title: [ 'PostgreSQL', 'Node.js', 'Ruby on Rails' ],
 		content: [
 			'PostgreSQL is a powerful, open source object-relational database system that uses and extends the SQL language',
 			'"JavaScript everywhere", unifying web-application development around a single programming language.',
 			'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.'
 		],
-		logos: ['postgresql.svg', 'node.svg', 'Ruby.svg']
+		logos: [ 'postgresql.svg', 'node.svg', 'rails.svg' ]
 	},
 	frontend: {
-		title: ['JavaScript (ES6)', 'Ruby on Rails', 'Sass'],
+		title: [ 'JavaScript (ES6)', 'HTML/CSS', 'Sass' ],
 		content: [
-			'Ajax calls, arrow functions, promises, event handling and more',
-			'Object oriented programming, ActiveRecord, Ruby gems',
+			'Dynamic interactions with the latest JS version. Including REST APIs and Ajax calls, arrow functions, promises, event handling and more.',
+			'Beautiful components and layouts designs by applying the latest HTML/CSS technologies, like CSS flexbox, grids and bootstrap.',
 			'CSS preprocessor which enables the use of functions, variables and eases browser compatibility'
 		],
-		logos: ['Javascript.svg', 'Rails.png', 'Sass.png']
+		logos: [ 'javascript.svg', 'css.svg', 'sass.svg' ]
 	},
 	ux: {
-		title: ['Full responsiveness', 'Wireframing and prototyping', 'Browser compatibility'],
+		title: [ 'Full responsiveness', 'Wireframing and prototyping', 'Browser   compatibility  ' ],
 		content: [
-			'Ajax calls, arrow functions, promises, event handling and more',
-			'Object oriented programming, ActiveRecord, Ruby gems',
-			'CSS preprocessor which enables the use of functions, variables and eases browser compatibility'
+			'Efficient and device-size independent designs by using media queries, CSS grid, Bootstrap framework and much more.',
+			'Creation of medium to high-fidelity mockups following the most recent web development practices.',
+			'There is nothing to worry about. Your scripts, CSS files and HTML codes will be fully functional on the different browsers available.'
 		],
-		logos: ['responsive.svg', 'prototype.svg', 'browser.svg']
+		logos: [ 'responsive.svg', 'prototype.svg', 'browser.svg' ]
 	},
 	management: {
-		title: ['Project planning', 'Version control', 'Deployment'],
+		title: [ 'Project execution', 'Version control', 'Deployment' ],
 		content: [
-			'Ajax calls, arrow functions, promises, event handling and more',
-			'Object oriented programming, ActiveRecord, Ruby gems',
-			'CSS preprocessor which enables the use of functions, variables and eases browser compatibility'
+			"From develop of product's target, unique value proposition, idea validation and user stories to deploy and pitch.",
+			'Complete understanding on github and git workflow for team collaboration and code version control.',
+			'Deployment of apps and websites using heroku and AWS as the main services.'
 		],
-		logos: ['management.svg', 'github.svg', 'deploy.svg']
+		logos: [ 'management.svg', 'github.svg', 'deploy.svg' ]
 	}
 };
 
 skills.forEach((skill) => {
-	skill.addEventListener('click', function (event) {
+	skill.addEventListener('click', function(event) {
+		skills.forEach((skill) => {
+			skill.classList.remove('active');
+		});
+		this.classList.toggle('active');
 		if (this.dataset.skill != lastClicked) {
 			cardArea.style.opacity = 0;
 			cardArea.addEventListener('transitionend', () => {
@@ -54,8 +58,7 @@ skills.forEach((skill) => {
 				cards.forEach((card) => {
 					card.querySelector('.skillcard__title').innerHTML = technologies[`${skill}`].title[i];
 					card.querySelector('.skillcard__content').innerHTML = technologies[`${skill}`].content[i];
-					console.log(`/app/images/${technologies[skill].logos[i]}`);
-					card.querySelector('.skillcard__label').src = `/app/images/${technologies[skill].logos[i]}`
+					card.querySelector('.skillcard__label').src = `/app/images/${technologies[skill].logos[i]}`;
 					i++;
 				});
 				cardArea.style.opacity = 1;
