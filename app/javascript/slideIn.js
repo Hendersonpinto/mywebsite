1c504e6cd04ac1cefc90b39c8096ecd9878d83cc
Henderson Pinto <hendersoncode@gmail.com>
2020-03-14 13:09:46 +0100

    Built the responsiveness of the website and changed the animation according to the screen size

--- app/javascript/slideIn.js
+++ app/javascript/slideIn.js
@@ -4,9 +4,11 @@ const projects = document.querySelectorAll('.project__wrapper')
 function checkSlide() {
     const bottomScreen = window.scrollY + window.innerHeight;
     projects.forEach((project) => {
-        console.dir(project)
         const halfProject = project.offsetParent.offsetTop + project.offsetTop + (project.offsetHeight / 4);
+        const slides = project.querySelectorAll('.slide')
+        console.dir(project)
         console.dir(project.children)
+        console.dir(project.querySelectorAll('.slide'))
         console.log(`the half is ${halfProject}`);
         // console.log(`The top of the screen is at ${topScreen}`);
         // console.log(`The bottom of the screen is at ${bottomScreen}`);
@@ -14,8 +16,9 @@ function checkSlide() {
         // console.log(`The height of the project is ${project.offsetHeight}`);
 
         if (bottomScreen > halfProject) {
-            project.classList.add('active');
-            [...project.children].forEach(child => child.classList.add('active'))
+            // project.classList.add('active');
+            // [...project.children].forEach(child => child.classList.add('active'))
+            slides.forEach(child => child.classList.add('active'))
         }
     });
 }
