04dd1cf3055857f2015a22908bc18e68d523534b
Henderson Pinto <hendersoncode@gmail.com>
2020-03-15 10:15:24 +0100

    Text animation now depends only on javascript and not on CSS keyframes

--- app/javascript/changeText2.js
+++ app/javascript/changeText2.js
@@ -1,19 +1,21 @@
 let messages = ["Web developer", "UX/UI Designer", "Team player", "Fullstack developer"];
 const text = document.querySelector('#banner__animated-text');
 
-function changeText() {
+function changeText(element) {
     let newMessage = messages.shift();
     messages.push(newMessage);
-    text.innerHTML = newMessage
+    element.innerHTML = newMessage
 }
-
-function delay(functionParameter) {
-    setInterval(functionParameter, 5000)
-}
-text.addEventListener('animationiteration', delay(changeText));
-
-
 
 function disappear(element) {
     element.style.opacity = 0;
+    setTimeout(() => {
+        changeText(element)
+        element.style.opacity = 1;
+    }, 2000)
 }
+
+disappear(text)
+setInterval(() => {
+    disappear(text)
+}, 5000)
